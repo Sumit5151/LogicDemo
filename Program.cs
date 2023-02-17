e11c7031e7d8970d2ac32e18c9caac2160ae22f9
Sumit5151 <sumitshitole5151@gmail.com>
2023-02-17 08:28:46 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//// 1 initialization
+//int i = 1;
+
+
+//while (i <= 12)                             //2 condition
+//{
+//    Console.Write(" A");
+//    i++;                    //3 increment or decrement
+//}
+
+
+
+
+
+
+//int j = 1;                          // 1 initialization
+
+//do
+//{
+//    Console.Write(" B");
+//    j++;                            //3 increment or decrement
+//} while (j <= 12);                  //2 condition
+
+
+
+
+//// 1 initialization                               //2 condition                //3 increment or decrement
+//for (int k = 0, a = 1, b = 2, c = 10; a <= 10 && k <= 12 && c <= 5; k++, a++, b--, c--)
+//{
+
+////}
+
+//int k;
+
+//// 1 initialization  //2 condition //3 increment or decrement
+//for (k = 0; k <= 12; k++)
+//{
+//    Console.Write("*");
+//}
+
+
+//Console.WriteLine("Value of K = "+ k);
+
+
+
+
+//int k;
+
+
+//for (k = 10; k >= 0; k--)
+//{
+//    Console.Write(" "+ k);
+//}
+
+
+//Console.WriteLine(" Value of K = " + k);
+
+
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.Write(" " + i);
+//}
+
+
+
+//Console.WriteLine();
+
+//for (int i = 5; i <= 50; i = i + 5)
+//{
+//    Console.Write(" " + i);
+//}
+
+
+
+////i = 11
+//for (int i = 1; i <= 10; i++)             //rows
+//{
+//    for (int j = 1; j <= 4; j++)             //colomns
+//    {
+//        Console.Write(" "+i);
+//    }
+
+//    Console.WriteLine();
+//}
+
+
+
+
+for (int i = 1; i <= 10; i++)             //rows
+{
+    for (int j = 1; j <= 5; j++)             //colomns
+    {
+        Console.Write(" " + j);
+    }
+
+    Console.WriteLine();
+}
+
+
+
+
+
